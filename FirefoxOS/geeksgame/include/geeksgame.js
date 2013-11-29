8be72c2051c2eb3c32de7367ae258206f543fbf3
kovacsv <kowik1984@gmail.com>
2013-11-30 00:06:55 +0100

    Add start button.

--- FirefoxOS/geeksgame/include/geeksgame.js
+++ FirefoxOS/geeksgame/include/geeksgame.js
@@ -1,35 +1,48 @@
+var pageHandler = null;
+
 function Restart ()
 {
 }
 
 function Resize ()
 {
-	var menu = document.getElementById ('mainmenu');
-	var content = document.getElementById ('maincontent');
+	var page = pageHandler.GetPage ();
+	var menus = document.getElementsByClassName ('menu');
+	var contents = document.getElementsByClassName ('content');
+
+	var menu = menus[page];
+	var content = contents[page];
+
 	var contentHeight = window.innerHeight - menu.clientHeight;
 	content.style.height = contentHeight + 'px';
 
-	var statusBar = document.getElementById ('statusbar');
-	var question = document.getElementById ('question');
-	var answer = document.getElementById ('answer0');
+	if (page === 0) {
+	} else if (page == 1) {
+		var statusBar = document.getElementById ('statusbar');
+		var question = document.getElementById ('question');
+		var answer = document.getElementById ('answer0');
 
-	var padding = 10;
-	var margin = 5;
-	
-	var answersHeight = 2 * answer.clientHeight + 3 * margin;
-	question.style.width = window.innerWidth + 'px';
-	question.style.height = (contentHeight - statusBar.clientHeight - answersHeight - 2 * padding) + 'px';
+		var padding = 10;
+		var margin = 5;
+		
+		var answersHeight = 2 * answer.clientHeight + 3 * margin;
+		question.style.width = window.innerWidth + 'px';
+		question.style.height = (contentHeight - statusBar.clientHeight - answersHeight - 2 * padding) + 'px';
 
-	var answers = document.getElementsByClassName ('answer');
-	var i;
-	for (i = 0; i < answers.length; i++) {
-		answer = answers[i];
-		answer.style.width = ((window.innerWidth - 4 * padding - 3 * margin) / 2.0) + 'px';
+		var answers = document.getElementsByClassName ('answer');
+		var i;
+		for (i = 0; i < answers.length; i++) {
+			answer = answers[i];
+			answer.style.width = ((window.innerWidth - 4 * padding - 3 * margin) / 2.0) + 'px';
+		}
 	}
 }
 
 function Load ()
 {
+	pageHandler = new PageHandler ();
+	pageHandler.SetToPage (0);
+
 	window.onresize = Resize;
 	Resize ();
 }
